539fadd2c806d89230cf3a23ffce936f1c74be21
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-21 15:48:12 +0530

    dd

---
+++ unary operator/positiveornagetive.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+    int x,y;
+    public:
+    complex(int a ,int b)
+    {
+        x=a;
+        y=b;
+    }
+    void display()
+    {
+        cout<<"the real part:- "<<x<<endl;
+        cout<<"the imaginary part:- "<<y<<endl;
+    }
+    complex operator-()
+    {
+        x=-x;
+        y=-y;
+    }
+};
+int main()
+{
+    complex c1(10,-20);
+    -c1;
+    c1.display();
+    return 0;
+}
