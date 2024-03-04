3db17a9f34fc97b821bba4083be67f58de713727
Dhaval Solanki <dhavalas24@gmail.com>
2024-03-04 16:06:12 +0530

    kk

---
+++ file management/ex.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    string st="skill 3 to 533";
+     string st2;
+    // ofstream out("semple.txt");
+    // out<<st;
+
+    ifstream in("semple.txt");
+    getline(in,st2);
+    cout<<st2;
+    return 0;
+
+}
